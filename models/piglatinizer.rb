5f8b7b51fb1667b2a272e3f16d90d0df12621849
Amil Masic <amil.masic@gmail.com>
2019-07-12 12:00:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,16 +12,14 @@ class PigLatinizer
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     consonants = alphabet - vowels
 
-    if vowels.include?(word[0])
-      word + "way"
-    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+    if consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
       word[3..-1] + word[0..2] + "ay"
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
       word[2..-1] + word[0..1] + "ay"
     elsif consonants.include?(word[0])
       word[1..-1] + word[0] + "ay"
-    else
-      word
+    else vowels.include?(word[0])
+      word + "way"
     end
 
   end
