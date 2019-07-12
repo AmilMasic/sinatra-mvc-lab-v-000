19308933fcdb90426956b66cad4e2201060c6a1c
Amil Masic <amil.masic@gmail.com>
2019-07-12 12:30:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,54 +1,21 @@
 class PigLatinizer
-  # attr_reader :word
 
-  #
-  # def piglatinize_sentence(sentence)
-  #   sentence_separated = sentence.split.collect
-  #   sentence_separated.map {|word| piglatinize(word)}.join(" ")
-  #
-  #
-  # end
-  #
-  # def piglatinize(word)
-  #
-  #   alphabet = ("a".."z").to_a
-  #   vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-  #   consonants = alphabet - vowels
-  #
-  #   if consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
-  #     word[3..-1] + word[0..2] + "ay"
-  #   elsif consonants.include?(word[0]) && consonants.include?(word[1])
-  #     word[2..-1] + word[0..1] + "ay"
-  #   elsif consonants.include?(word[0])
-  #     word[1..-1] + word[0] + "ay"
-  #    else #vowels.include?(word[0])
-  #     word + "way"
-  #   end
-  #
-  # end
 
   def translate_word(text)
 
     if /^[aeiou]/i.match(text)
-
       "#{text}way"
-
     else
-
       parts = text.split(/([aeiou].*)/)
-
       "#{parts[1]}#{parts[0]}ay"
 
     end
 
   end
 
-
-
   def piglatinize(text)
 
     words = text.split(' ')
-
     words.map {|word| translate_word(word)}.join(' ')
 
   end
