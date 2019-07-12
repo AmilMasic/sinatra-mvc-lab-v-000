af191c373f2ac572ff028117c58b6511a0e2dd3c
Amil Masic <amil.masic@gmail.com>
2019-07-12 12:19:03 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -21,7 +21,7 @@ class PigLatinizer
       word[2..-1] + word[0..1] + "ay"
     elsif consonants.include?(word[0])
       word[1..-1] + word[0] + "ay"
-    else vowels.include?(word[0])
+     else #vowels.include?(word[0])
       word + "way"
     end
 
