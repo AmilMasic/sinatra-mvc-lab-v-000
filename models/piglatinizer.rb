df886093f88af0ca08e987512bd166e98e7084cb
Amil Masic <amil.masic@gmail.com>
2019-07-12 12:21:39 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -19,7 +19,7 @@ class PigLatinizer
       word[3..-1] + word[0..2] + "ay"
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
       word[2..-1] + word[0..1] + "ay"
-    else consonants.include?(word[0])
+    elsif consonants.include?(word[0])
       word[1..-1] + word[0] + "ay"
      else #vowels.include?(word[0])
       word + "way"
