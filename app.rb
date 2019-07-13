72869a1b8a5bea00e7da1cda75d31c0927f3c14e
Amil Masic <amil.masic@gmail.com>
2019-07-13 09:59:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @analyzed_text = PigLatinizer.new(params[:user_phrase])
+    @analyzed_text = PigLatinizer.piglatinize(params[:user_phrase])
 
     erb :pig_latin_phrase
   end
